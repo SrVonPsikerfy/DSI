0110a89f2f830ca0318bf2499a513c27b6db81de
mihern07 <mihern07@ucm.es>
2021-05-09 20:05:38 +0200

    Prototipo inicial de abilities

--- DSIProyectoFinal/Knights.cs
+++ DSIProyectoFinal/Knights.cs
@@ -8,5 +8,42 @@ namespace DSIProyectoFinal
 {
     class Knights
     {
+        public string Name { get; set; }
+        public string ImageLocation { get; set; }
+        public string Role { get; set; }
+        public int Cost { get; set; }
+        public int AtkPhy { get; set; }
+        public int AtkMag { get; set; }
+        public int Mana { get; set; }
+        public int DefPhy { get; set; }
+        public int DefMag { get; set; }
+        public int Range { get; set; }
+        public int Lvl { get; set; }
+        public int CurrentExp { get; set; }
+        public int MaxExp { get; set; }
+        public int PointsAvailable { get; set; }
+        public bool[] Abilities { get; set; }
+        public int[] EquipedAbilites { get; set; }
+
+        public Knights(string name, string imageLocation, string role, int cost, int atkPhy, int atkMag, int mana, int defPhy, int defMag,
+            int range, int lvl, int currentExp, int maxExp, int pointsAvailable, bool[] abilities, int[] equipedAbilities)
+        {
+            Name = name;
+            ImageLocation = imageLocation;
+            Role = role;
+            Cost = cost;
+            AtkPhy = atkPhy;
+            AtkMag = atkMag;
+            Mana = mana;
+            DefPhy = defPhy;
+            DefMag = defMag;
+            Range = range;
+            Lvl = lvl;
+            CurrentExp = currentExp;
+            MaxExp = maxExp;
+            PointsAvailable = pointsAvailable;
+            Abilities = abilities;
+            EquipedAbilites = equipedAbilities;
+        }
     }
 }
