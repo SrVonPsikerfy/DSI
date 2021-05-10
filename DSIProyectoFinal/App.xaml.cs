b850af414874333704032150a030b2b34507cc05
AaronNauzetMorenoSosa <aarmor01@ucm.es>
2021-05-10 22:15:04 +0200

    Almost finished TeamSelect

--- DSIProyectoFinal/App.xaml.cs
+++ DSIProyectoFinal/App.xaml.cs
@@ -66,7 +66,7 @@ namespace DSIProyectoFinal
                     // Cuando no se restaura la pila de navegación, navegar a la primera página,
                     // configurando la nueva página pasándole la información requerida como
                     //parámetro de navegación
-                    rootFrame.Navigate(typeof(Store), e.Arguments);
+                    rootFrame.Navigate(typeof(TeamConfig), e.Arguments);
                 }
                 // Asegurarse de que la ventana actual está activa.
                 Window.Current.Activate();
