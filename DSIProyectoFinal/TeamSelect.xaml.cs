ad6d15d49e478a1606d8fe553421936daf717d55
Samuel <samuebla@ucm.es>
2021-05-11 01:47:22 +0200

    Me quiero cortar las venas pt.2

--- DSIProyectoFinal/TeamSelect.xaml.cs
+++ DSIProyectoFinal/TeamSelect.xaml.cs
@@ -26,5 +26,10 @@ namespace DSIProyectoFinal
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
